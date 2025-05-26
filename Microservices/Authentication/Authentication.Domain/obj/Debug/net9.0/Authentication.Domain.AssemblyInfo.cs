09e8f98fd025c7cd9f376fc317c4035930b9d521
mohitpawar4102 <mohit.pawar@candentech.com>
2025-05-26 16:11:11 +0530

    Latest

--- Microservices/Authentication/Authentication.Domain/obj/Debug/net9.0/Authentication.Domain.AssemblyInfo.cs
+++ Microservices/Authentication/Authentication.Domain/obj/Debug/net9.0/Authentication.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8da32531a03d7f704a8b62b4b7734d393069365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4975bbf83331a8d12566d80a117615ed2cb9f027")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
