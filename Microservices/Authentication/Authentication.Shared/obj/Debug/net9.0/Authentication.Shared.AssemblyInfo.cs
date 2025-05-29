9d5776f237235dc2d430a97a3bc37f899027a918
mohitpawar4102 <mohit.pawar@candentech.com>
2025-05-29 11:26:46 +0530

    Lastest Code

--- Microservices/Authentication/Authentication.Shared/obj/Debug/net9.0/Authentication.Shared.AssemblyInfo.cs
+++ Microservices/Authentication/Authentication.Shared/obj/Debug/net9.0/Authentication.Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Authentication.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e8f98fd025c7cd9f376fc317c4035930b9d521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c3a79677feeec3577964af0b2bb7585a0e7e499")]
 [assembly: System.Reflection.AssemblyProductAttribute("Authentication.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Authentication.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Microservices/ContentCreator/ContentCreator.Tests/obj/Debug/net9.0/ContentCreator.Tests.AssemblyInfo.cs
+++ Microservices/ContentCreator/ContentCreator.Tests/obj/Debug/net9.0/ContentCreator.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContentCreator.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09e8f98fd025c7cd9f376fc317c4035930b9d521")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c94e57f025492eed35f4567e82735af57e0547")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContentCreator.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContentCreator.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
